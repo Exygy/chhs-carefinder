d985d17eba2ccc8e49bbb93f14cf465e1c082ddd
Matt Luedke <mluedke2@gmail.com>
2016-05-24 16:00:15 -0700

    [#120005175] - favorites nav bar passes query parameter

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,7 +28,7 @@ export class NavBar extends React.Component {
     let loggedInNavLinks = (
       <ul className='menu' role='menubar'>
         {searchFacilitiesLink}
-        <li role='menuitem'><Link to='/search/favorites'>My Facilities</Link></li>
+        <li role='menuitem'><Link to={{ pathname: '/search', query: { favorites: true } }}>My Facilities</Link></li>
         <li role='menuitem'><Link to='/profile'>My Profile</Link></li>
         <li role='menuitem'><Link to='/messages'>My Messages</Link></li>
         <li role='menuitem'><Link onClick={this.logOut} to='/'>Logout</Link></li>
