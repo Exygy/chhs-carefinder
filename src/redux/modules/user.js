0803cf17c57bc109dd0232ce3cd3d4d8a88485cb
Matt Luedke <mluedke2@gmail.com>
2016-05-31 13:44:45 -0700

    [#119778667] - auth performs mock login in reducer

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -4,14 +4,28 @@ import { createAction, handleActions } from 'redux-actions'
 // Actions
 // ------------------------------------
 export const userLoad = createAction('USER_LOAD')
+export const login = () => {
+  return (dispatch, getState) => {
+    dispatch(userLoad({firstName: 'Matt', lastName: 'Matty'}))
+  }
+}
 
 export const actions = {
-  userLoad
+  userLoad,
+  login
 }
 
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
+
+/*
+loggedInUser example: {
+  firstName: 'Matt',
+  lastName: 'Luedke'
+}
+*/
+
 const INITIAL_STATE = {
   favorites: [],
   loggedInUser: {}
--- src/views/AuthView.jsx
+++ src/views/AuthView.jsx
@@ -3,29 +3,19 @@ import { connect } from 'react-redux'
 import { actions } from 'redux/modules/user'
 import { Link } from 'react-router'
 
-const mapStateToProps = (state) => ({
-  userLoad: state.facilities.filterByFavorites
-})
-
 export class AuthView extends React.Component {
 
   static propTypes = {
-    userLoad: PropTypes.func.isRequired
-  }
-
-  loadFakeUser = () => {
-    console.log('loadingFakeUser')
-    this.props.userLoad({name: 'Matt'})
-    console.log('fakeUserLoaded')
+    login: PropTypes.func.isRequired
   }
 
   render () {
     return (
       <div className='content with-sticky-header'>
-        <Link onClick={this.loadFakeUser} to='/'>Sign in</Link>
+        <Link onClick={this.props.login} to='/'>Sign in</Link>
       </div>
     )
   }
 }
 
-export default connect(mapStateToProps, actions)(AuthView)
+export default connect(null, actions)(AuthView)
