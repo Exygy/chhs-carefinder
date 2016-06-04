af11fd76d234eb0f3f5f6fe4d6402a23ae4e8e02
Matt Luedke <mluedke2@gmail.com>
2016-06-03 17:48:29 -0700

    [#120873855] - use foster end date when present

--- src/components/profile/FosterChildBox.jsx
+++ src/components/profile/FosterChildBox.jsx
@@ -5,8 +5,18 @@ export class FosterChildBox extends React.Component {
     fosterChild: PropTypes.object.isRequired
   }
 
+  get fosterDate () {
+    let { fosterEndDate, fosterStartDate } = this.props.fosterChild
+
+    if (fosterEndDate.length > 0) {
+      return <li className='meta-list-item'>Fostered Until: {fosterEndDate}</li>
+    } else {
+      return <li className='meta-list-item'>Fostering Since: {fosterStartDate}</li>
+    }
+  }
+
   render () {
-    let { birthday, caseId, firstName, fosterStartDate, image, lastName } = this.props.fosterChild
+    let { birthday, caseId, firstName, image, lastName } = this.props.fosterChild
     return (
       <div className='media-object'>
         <div className='media-object-section'>
@@ -18,7 +28,7 @@ export class FosterChildBox extends React.Component {
             <li className='meta-list-item t-caps'>{firstName} {lastName}</li>
             <li className='meta-list-item'>Case ID: {caseId}</li>
             <li className='meta-list-item'>Birthday: {birthday}</li>
-            <li className='meta-list-item'>Fostering Since: {fosterStartDate}</li>
+            {this.fosterDate}
           </ul>
         </div>
       </div>
