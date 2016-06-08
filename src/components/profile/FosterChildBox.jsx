a77da5b37c4f3df91778c0bf078fa4e8389c5a64
Matt Luedke <mluedke2@gmail.com>
2016-06-08 13:02:59 -0700

    [#121157669] - reword foster child dates

--- src/components/profile/FosterChildBox.jsx
+++ src/components/profile/FosterChildBox.jsx
@@ -9,7 +9,7 @@ export class FosterChildBox extends React.Component {
     let { fosterEndDate, fosterStartDate } = this.props.fosterChild
 
     if (fosterEndDate.length > 0) {
-      return <li className='meta-list-item'><strong>Fostered Until</strong>: {fosterEndDate}</li>
+      return <li className='meta-list-item'><strong>Fostered From</strong>: {fosterStartDate} - {fosterEndDate}</li>
     } else {
       return <li className='meta-list-item'><strong>Fostering Since</strong>: {fosterStartDate}</li>
     }
