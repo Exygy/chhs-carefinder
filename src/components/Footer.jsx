3212428314227eab48f0f7811211d3194cab0c19
Matt Luedke <mluedke2@gmail.com>
2016-06-09 13:53:04 -0700

    [#121255165] - updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,30 +7,27 @@ export class Footer extends React.Component {
   render () {
     return (
       <footer className='footer stuck is-at-bottom'>
-        <div className='row'>
-          <div className='medium-4 columns'>
-            <div className='row'>
-              <div className='medium-2 columns'>
-                <img src={require('../images/seal_ca_bw.png')} alt='California Seal' />
-              </div>
-              <div className='medium-10 columns'>
+        <div className='row center'>
+          <div className='medium-4 medium-centered columns'>
+            <div className='row center'>
+              <img src={require('../images/seal_ca_bw.png')} alt='California Seal' />
+            </div>
+            <div className='row center'>
+              <div className='medium-6 columns'>
                 <p className='footer-copywrite'>
-                  Copyright &copy; 2016 State of CA
+                  Copyright &copy; 2016<br />
+                  State of CA
+                </p>
+              </div>
+              <div className='medium-6 columns'>
+                <p className='footer-contact'>
+                  1-800-KIDS-4-US<br />
+                  <a href='http://www.cdss.ca.gov/cdssweb/privacy.htm' target='_blank' className='footer-link'>
+                    Privacy Policy
+                  </a>
                 </p>
               </div>
             </div>
-          </div>
-          <div className='medium-4 columns'>
-            <p className='footer-links'>
-              <a href='http://www.cdss.ca.gov/cdssweb/privacy.htm' target='_blank' className='footer-link'>
-                Privacy Policy
-              </a>
-            </p>
-          </div>
-          <div className='medium-4 columns'>
-            <p className='footer-contact'>
-              1-800-KIDS-4-US (1-800-543-7487)
-            </p>
           </div>
         </div>
       </footer>
